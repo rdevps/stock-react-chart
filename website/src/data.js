380a1bbfa05a12a1afd30a8474195e4000733b2c
rdevps <psdevu@gmail.com>
2025-06-05 10:23:39 +0530

    Add website wrapper with demo chart

---
+++ website/src/data.js
@@ -0,0 +1,11 @@
+const data = [
+  { date: '2025-01-01', close: 100 },
+  { date: '2025-01-02', close: 105 },
+  { date: '2025-01-03', close: 102 },
+  { date: '2025-01-04', close: 108 },
+  { date: '2025-01-05', close: 110 },
+  { date: '2025-01-06', close: 115 },
+  { date: '2025-01-07', close: 112 }
+];
+
+export default data;
